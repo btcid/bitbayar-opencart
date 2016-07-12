d903575c802971c855395d89e68645f9f1ce51fb
Teddy Fresnel <teddyfresnel@gmail.com>
2016-07-12 10:02:13 +0800

    Update payment cancel template v 2.2.0

--- catalog/controller/payment/bitbayar.php
+++ catalog/controller/payment/bitbayar.php
@@ -269,7 +269,7 @@ class ControllerPaymentBitbayar extends Controller {
 		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/common/success.tpl')) {
 			$this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/common/success.tpl', $data));
 		} else {
-			$this->response->setOutput($this->load->view('default/template/common/success.tpl', $data));
+			$this->response->setOutput($this->load->view('common/success.tpl', $data));
 		}
 	}
 }
